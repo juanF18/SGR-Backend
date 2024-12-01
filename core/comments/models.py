5361932641f12be007e78e3455d53c96291c62fa
Juan Felipe Cortes <juanfelipecortescastrillon@gmail.com>
2024-12-01 15:02:36 -0500

    fix: error in password check in login

--- core/comments/models.py
+++ core/comments/models.py
@@ -5,9 +5,13 @@ from core.users.models import User
 
 # Create your models here.
 class Comment(models.Model):
-    id = models.UUIDField("id", primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    id = models.UUIDField(
+        "id", primary_key=True, default=uuid.uuid4, editable=False, unique=True
+    )
     comment_text = models.TextField("comment_text", null=True, blank=True)
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="comments", null=True
+    )
     created_at = models.DateTimeField("created_at", auto_now_add=True)
     updated_at = models.DateTimeField("updated_at", auto_now=True)
     deleted_at = models.DateTimeField("deleted_at", null=True, blank=True)
