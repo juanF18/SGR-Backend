dbd23b0a74c41f80388841569873cf7696c84907
Juan Felipe Cortes <juanfelipecortescastrillon@gmail.com>
2024-12-04 17:42:00 -0500

    feat: add initial part for projects endpoint

--- core/projects/models.py
+++ core/projects/models.py
@@ -1,25 +1,33 @@
 import uuid
 from django.db import models
+from django.utils import timezone
+from django.core.validators import FileExtensionValidator
 from core.entities.models import Entity
 
 
 # Create your models here.
 class Project(models.Model):
-    id = models.UUIDField("id", primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    id = models.UUIDField(
+        "id", primary_key=True, default=uuid.uuid4, editable=False, unique=True
+    )
     name = models.CharField("name", max_length=100, blank=True, null=True)
     description = models.TextField("description", blank=True, null=True)
     value = models.DecimalField("value", max_digits=10, decimal_places=2, default=0)
-    start_date = models.DateField("start_date", blank=True, null=True)
-    end_date = models.DateField("end_date", blank=True, null=True)
-    file_budget_url = models.CharField(
-        "file_budget_url", max_length=150, blank=True, null=True
+    start_date = models.DateField("start_date", null=True, default=timezone.now)
+    end_date = models.DateField("end_date", null=True, default=timezone.now)
+    file_budget = models.FileField(
+        "file_budget",
+        upload_to="projects/budgets/",
+        null=True,
+        validators=[FileExtensionValidator(allowed_extensions=["xlsx"])],
     )
-    file_activities_url = models.CharField(
-        "file_activities_url", max_length=150, blank=True, null=True
+    file_activities = models.FileField(
+        "file_activities",
+        upload_to="projects/activities/",
+        null=True,
+        validators=[FileExtensionValidator(allowed_extensions=["xlsx"])],
     )
-    entity = models.ForeignKey(
-        Entity, on_delete=models.SET_NULL, null=True, blank=True
-    )
+    entity = models.ForeignKey(Entity, on_delete=models.SET_NULL, null=True, blank=True)
     created_at = models.DateTimeField("created_at", auto_now_add=True)
     updated_at = models.DateTimeField("updated_at", auto_now=True)
     deleted_at = models.DateTimeField("deleted_at", blank=True, null=True)
