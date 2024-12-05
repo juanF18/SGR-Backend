60d0c1b9ae4bf4d8b4221ed64a0f100082fc59a3
CristianHenao <cristianhenao88@gmail.com>
2024-12-04 23:34:47 -0500

    feat: add endpoint get by project

--- core/rubros/urls.py
+++ core/rubros/urls.py
@@ -3,5 +3,12 @@ from . import views
 
 urlpatterns = [
     path("rubros", views.RubroView.as_view(), name="rubros_view"),
-    path("rubros/<uuid:id>/", views.RubroDetailView.as_view(), name="rubro_detail_view"),
+    path(
+        "rubros/<uuid:id>/", views.RubroDetailView.as_view(), name="rubro_detail_view"
+    ),
+    path(
+        "rubros/<uuid:project_id>/",
+        views.RubroProjectView.as_view(),
+        name="rubro_project_view",
+    ),
 ]
