84d7e7fba182fc6e20a8ad0714d7d1a76ff9089c
Juan Felipe Cortes <juanfelipecortescastrillon@gmail.com>
2024-12-11 12:00:52 -0500

    fix: solve url problem with filter task by activity_id

--- core/tasks/urls.py
+++ core/tasks/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
         "tasks/<uuid:task_id>", views.TaskDetailView.as_view(), name="task_detail_view"
     ),
     path(
-        "tasks/?activity_id=<uuid:activity_id>",
+        "tasks/activity/<uuid:activity_id>",
         views.TaskByActivityView.as_view(),
         name="tasks_by_activity_view",
     ),
