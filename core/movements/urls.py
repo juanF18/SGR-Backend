6e61f1652265fe95032ddba19950e61668402ecb
Juan Felipe Cortes <juanfelipecortescastrillon@gmail.com>
2024-12-13 01:31:54 -0500

    feat: add a enpoint to movements

--- core/movements/urls.py
+++ core/movements/urls.py
@@ -4,8 +4,18 @@ from . import views
 urlpatterns = [
     path("movements", views.MovementView.as_view(), name="movements_view"),
     path(
-        "movements/<uuid:id>/",
+        "movements/<uuid:id>",
         views.MovementDetailView.as_view(),
         name="movement_detail_view",
+    ),
+    path(
+        "movements/project/<uuid:project_id>",
+        views.MovementsByProjectId.as_view(),
+        name="movement-by-project",
+    ),
+    path(
+        "movements/sum_by_project/<uuid:project_id>",
+        views.MovementsSumByProjectId.as_view(),
+        name="movements-sum-by-project",
     ),
 ]
