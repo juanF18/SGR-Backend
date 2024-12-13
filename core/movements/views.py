3ff4c4e50f038fb9829867518a530110fd7c3d06
Juan Felipe Cortes <juanfelipecortescastrillon@gmail.com>
2024-12-13 01:31:54 -0500

    feat: add a enpoint to movements

--- core/movements/views.py
+++ core/movements/views.py
@@ -1,6 +1,8 @@
 from rest_framework import status
 from rest_framework.response import Response
+from core.activities.models import Activity
 from .models import Movement
+from django.db.models import Sum
 from rest_framework.views import APIView
 from .serializers import MovementSerializer
 from core.cdps.models import Cdps
@@ -230,6 +232,126 @@ class MovementDetailView(APIView):
         except Exception as e:
             response = {
                 "message": f"Error eliminando el movimiento: {str(e)}",
+                "status": status.HTTP_500_INTERNAL_SERVER_ERROR,
+            }
+            return Response(response, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class MovementsByProjectId(APIView):
+    """
+    Class to handle HTTP requests related to movements
+
+    @methods:
+    - get: Get all movements
+    - post: Create a new movement
+    - get_movements_by_project: Get movements filtered by project ID
+    """
+
+    # Endpoint para obtener movimientos por proyecto
+    @swagger_auto_schema(
+        operation_description="Obtener movimientos filtrados por ID de proyecto",
+        responses={
+            200: openapi.Response(
+                description="Movimientos recuperados correctamente",
+                schema=MovementSerializer(many=True),
+            ),
+            400: openapi.Response(description="Proyecto no encontrado"),
+            500: openapi.Response(description="Error interno del servidor"),
+        },
+    )
+    def get(self, request, project_id):
+        """
+        Obtener movimientos filtrados por el ID del proyecto.
+        @param request: HTTP request
+        @param project_id: ID del proyecto
+        @return: JSON response con los movimientos del proyecto
+        """
+        try:
+            # Filtramos las actividades relacionadas con el proyecto
+            activities = Activity.objects.filter(project_id=project_id)
+
+            # Obtenemos los CDPs relacionados con esas actividades
+            cdps_ids = Cdps.objects.filter(activity__in=activities).values_list(
+                "id", flat=True
+            )
+
+            # Filtramos los movimientos relacionados con los CDPs obtenidos
+            movements = Movement.objects.filter(cdp_id__in=cdps_ids)
+
+            # Serializamos los movimientos encontrados
+            movement_serializer = MovementSerializer(movements, many=True)
+
+            return Response(movement_serializer.data, status=status.HTTP_200_OK)
+
+        except Activity.DoesNotExist:
+            response = {
+                "message": "Proyecto no encontrado",
+                "status": status.HTTP_400_BAD_REQUEST,
+            }
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            response = {
+                "message": f"Error obteniendo los movimientos: {str(e)}",
+                "status": status.HTTP_500_INTERNAL_SERVER_ERROR,
+            }
+            return Response(response, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class MovementsSumByProjectId(APIView):
+    """
+    Clase para manejar solicitudes HTTP relacionadas con la suma de movimientos por ID de proyecto
+    """
+
+    @swagger_auto_schema(
+        operation_description="Obtener la suma total de los movimientos filtrados por ID de proyecto",
+        responses={
+            200: openapi.Response(
+                description="Suma de movimientos obtenida correctamente",
+                schema=openapi.Schema(
+                    type=openapi.TYPE_OBJECT,
+                    properties={
+                        "total_amount": openapi.Schema(
+                            type=openapi.TYPE_NUMBER, format=openapi.FORMAT_FLOAT
+                        )
+                    },
+                ),
+            ),
+            400: openapi.Response(description="Proyecto no encontrado"),
+            500: openapi.Response(description="Error interno del servidor"),
+        },
+    )
+    def get(self, request, project_id):
+        """
+        Obtener la suma total de los movimientos para un proyecto dado, filtrando por el ID del proyecto.
+        @param request: HTTP request
+        @param project_id: ID del proyecto
+        @return: JSON response con la suma total de los movimientos
+        """
+        try:
+            # Filtramos las actividades relacionadas con el proyecto
+            activities = Activity.objects.filter(project_id=project_id)
+
+            if not activities.exists():
+                return Response(
+                    {"message": "Proyecto no encontrado"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+            # Obtenemos los CDPs relacionados con esas actividades
+            cdps_ids = Cdps.objects.filter(activity__in=activities).values_list(
+                "id", flat=True
+            )
+
+            # Filtramos los movimientos relacionados con esos CDPs
+            movements = Movement.objects.filter(cdp_id__in=cdps_ids)
+
+            # Calculamos la suma total de los movimientos
+            total_amount = movements.aggregate(Sum("amount"))["amount__sum"] or 0
+
+            return Response({"total_amount": total_amount}, status=status.HTTP_200_OK)
+        except Exception as e:
+            response = {
+                "message": f"Error obteniendo la suma de los movimientos: {str(e)}",
                 "status": status.HTTP_500_INTERNAL_SERVER_ERROR,
             }
             return Response(response, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
