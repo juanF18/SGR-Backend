9bf8a59a7ab2eb2d0161f4c05afa3c0b8e04e164
CristianHenao <cristianhenao88@gmail.com>
2024-12-16 06:18:11 -0500

    feat: add counterpart execution

--- core/counterparts/models.py
+++ core/counterparts/models.py
@@ -1,20 +1,22 @@
 import uuid
 from django.db import models
-from core.rubros.models import Rubro
+from core.projects.models import Project
 
 
 # Create your models here.
 class Counterpart(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False, unique=True
+    )
     name = models.CharField(max_length=100)
     value_species = models.DecimalField(
-        "value_species", max_digits=10, decimal_places=2, default=0
+        "value_species", max_digits=20, decimal_places=2, default=0
     )
     value_chash = models.DecimalField(
-        "value_chash", max_digits=10, decimal_places=2, default=0
+        "value_chash", max_digits=20, decimal_places=2, default=0
     )
-    rubro = models.ForeignKey(
-        Rubro, on_delete=models.SET_NULL, null=True, blank=True
+    project = models.ForeignKey(
+        Project, on_delete=models.SET_NULL, null=True, blank=True
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
